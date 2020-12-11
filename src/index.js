fb1afa93a69f4f966a5deca05d825a0f3b597828
jayroivan <ma0721032017@unab.edu.sv>
2020-12-11 13:08:44 -0600

    Final

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const elementw = 
+<BrowserRouter>
+<App />
+</BrowserRouter>;
 const element = <h1>Hello, world</h1>;
 
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(elementw, element, document.getElementById('root'));
 
 serviceWorker.unregister();

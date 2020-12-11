b1e2c1097bab8a9455288757f9876ed69b3e1b2c
jayroivan <ma0721032017@unab.edu.sv>
2020-12-11 13:16:00 -0600

    Final

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import Indexw from './components/index.componentw';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
   <App />
-  <Indexw />
   </BrowserRouter>, document.getElementById('root'));
 
 serviceWorker.unregister();

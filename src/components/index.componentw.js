709c84eea012d6dfa1fc3bb098aa55d2482811a8
jayroivan <ma0721032017@unab.edu.sv>
2020-12-17 14:18:09 -0600

    Final

--- src/components/index.componentw.js
+++ src/components/index.componentw.js
@@ -10,7 +10,22 @@ export default class Index extends Component {
       return (
         <div>
           <h3 align="center">Pagos y Cobros Seguros... Alguien en quien confiar, justo, personal y sencillo.</h3>
-          
+                    <div>
+                    <img
+                      src={require('../img/pay.png')}
+                      alt="profile-img"
+                      className="profile-img-card"
+                      width="80%"
+                    />
+                    </div>
+                    <div>
+                    <img
+                      src={require('../img/pays.png')}
+                      alt="profile-img"
+                      className="profile-img-card"
+                      width="80%"
+                    />
+                    </div>
         </div>
       );
     }
